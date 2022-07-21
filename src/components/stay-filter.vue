ad2dd12ea4504eebf74abcd64183222957fcb5cb
ערן פסקל <eranpascal@gamil.com>
2022-07-21 12:48:43 +0300

    large filter form skelton

--- src/components/stay-filter.vue
+++ src/components/stay-filter.vue
@@ -15,44 +15,69 @@
   <form class="large-header-filter flex">
     <div class="destination-input">
       <label for="destination-input">Where</label>
-      <input name="destination-input" type="text" placeholder="Search detinations">
+      <input v-model="filter.destination" name="destination-input" type="text" placeholder="Search detinations">
     </div>
     <span>|</span>
     <div class="checkIn-input">
       <label for="checkIn-input">Check in</label>
-      <button class="checkIn-input">Add Dates</button>
+      <button v-if="checkinDisplayed" :class="{ active: checkinDisplayed }" @click="checkinDisplayed = !checkinDisplayed">Add
+        Dates </button>
+      <flat-pickr class="checkIn-input-picker" v-else v-model="date.checkIn"></flat-pickr>
     </div>
-    <span>|</span>
+      <span>|</span>
     <div class="checkOut-input">
-      <label for="checkOut-input">Checck out</label>
-      <button class="checkOut-input">Add Dates</button>
+      <label for="checkOut-input">Check out</label>
+      <button v-if="checkOutDisplayed" :class="{ active: checkOutDisplayed }" @click="checkOutDisplayed = !checkOutDisplayed">Add
+        Dates </button>
+      <flat-pickr v-else v-model="date.checkOut"></flat-pickr>
     </div>
     <span>|</span>
     <div class="gusets-input flex">
       <div class="gusets-btn-warper">
-      <label for="gusets-input">Who</label>
-      <button class="gusets-input">Add gusets</button>
-      </div>
-        <button class="large-filter-search"><img src="../styles/icons/search_white.png" alt="">Search</button>
+        <label for="gusets-input">Who</label>
+        <button class="gusets-input">Add gusets</button>
+      <button class="large-filter-search"><img src="../styles/icons/search_white.png" alt="">Search</button>
     </div>
-
-
-
-
+      </div>
   </form>
+        <!-- <div class="add-modal">
+          <div>
+            content of the popover
+  <ul>input</ul>
+  <ul>input</ul>
+  <ul>input</ul>
+          </div>
+        </div> -->
 </template>
 <script>
+import flatPickr from 'vue-flatpickr-component';
+import 'flatpickr/dist/flatpickr.css';
+
 export default {
   props: {
 
   },
   components: {
+    flatPickr
 
   },
   data() {
-    return {};
+    return {
+      filter: {
+        destination: '',
+        numOfBeds: 0,
+      },
+      date:{
+        checkIn: null,
+        checkOut: null,
+      },
+      checkinDisplayed: true,
+      checkOutDisplayed: true
+    };
   },
-  methods: {},
+  methods: {
+
+  },
   actions: {},
   computed: {},
   created() { },
