0b48be71554aeb7b7d6730dc8446ce026196e618
ערן פסקל <eranpascal@gamil.com>
2022-07-21 16:14:23 +0300

    header filter modal start

--- src/components/stay-filter.vue
+++ src/components/stay-filter.vue
@@ -20,45 +20,68 @@
     <span>|</span>
     <div class="checkIn-input">
       <label for="checkIn-input">Check in</label>
-      <button v-if="checkinDisplayed" :class="{ active: checkinDisplayed }" @click="checkinDisplayed = !checkinDisplayed">Add
+      <button v-if="checkinDisplayed" :class="{ active: checkinDisplayed }"
+        @click="checkinDisplayed = !checkinDisplayed">Add
         Dates </button>
       <flat-pickr class="checkIn-input-picker" v-else v-model="date.checkIn"></flat-pickr>
     </div>
-      <span>|</span>
+    <span>|</span>
     <div class="checkOut-input">
       <label for="checkOut-input">Check out</label>
-      <button v-if="checkOutDisplayed" :class="{ active: checkOutDisplayed }" @click="checkOutDisplayed = !checkOutDisplayed">Add
+      <button v-if="checkOutDisplayed" :class="{ active: checkOutDisplayed }"
+        @click="checkOutDisplayed = !checkOutDisplayed">Add
         Dates </button>
       <flat-pickr v-else v-model="date.checkOut"></flat-pickr>
     </div>
+
     <span>|</span>
     <div class="gusets-input flex">
-      <div class="gusets-btn-warper">
-        <label for="gusets-input">Who</label>
-        <button class="gusets-input">Add gusets</button>
-      <button class="large-filter-search"><img src="../styles/icons/search_white.png" alt="">Search</button>
-    </div>
+ <Popper>
+   <div class="gusets-btn-warper">
+          <label for="gusets-input">Who</label>
+          <button class="gusets-input">Add gusets</button>
+        </div>
+    <template #content>
+      <div class="gusets-modal">
+          <num-input></num-input>
+          <ul>input</ul>
+          <!-- <ul>input</ul> -->
+          <!-- <ul>input</ul> -->
       </div>
-  </form>
-        <!-- <div class="add-modal">
+    </template>
+  </Popper>
+          <!--
+     
           <div>
-            content of the popover
-  <ul>input</ul>
-  <ul>input</ul>
-  <ul>input</ul>
-          </div>
-        </div> -->
+          <ul>input</ul>
+          <ul>input</ul>
+          <ul>input</ul> -->
+    </div>
+
+      
+
+      <button class="large-filter-search"><img src="../styles/icons/search_white.png" alt="">Search</button>
+   
+  </form>
+
+
 </template>
 <script>
 import flatPickr from 'vue-flatpickr-component';
 import 'flatpickr/dist/flatpickr.css';
+import { defineComponent } from "vue";
+import Popper from "vue3-popper";
+import numInput from "./num-input.vue"
+
 
 export default {
   props: {
 
   },
   components: {
-    flatPickr
+    flatPickr,
+    Popper,
+    numInput
 
   },
   data() {
@@ -67,12 +90,13 @@ export default {
         destination: '',
         numOfBeds: 0,
       },
-      date:{
+      date: {
         checkIn: null,
         checkOut: null,
       },
       checkinDisplayed: true,
-      checkOutDisplayed: true
+      checkOutDisplayed: true,
+      modalActive: true,
     };
   },
   methods: {
