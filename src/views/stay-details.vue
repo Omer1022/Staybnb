a26497c7be7290c9b887a7a42b176bb0596877b0
Omer Dahan <92370133+Omer1022@users.noreply.github.com>
2022-07-25 10:44:20 +0300

    Add Users

--- src/views/stay-details.vue
+++ src/views/stay-details.vue
@@ -212,11 +212,7 @@
                 </button>
               </div>
               <fancy-btn class="reserve-btn">Reserve</fancy-btn>
-<<<<<<< HEAD
               <!-- <button class="reserve-btn">
-=======
-              <!-- <button class="">
->>>>>>> 153d8fd4ee3aa5e6ec2d807dc773d96f6c97eafe
                 <span>Reserve</span>
               </button> -->
             </form>
