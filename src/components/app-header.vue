99295a14ed136fcddff29a5739704b12ecf1a8ac
Omer Dahan <92370133+Omer1022@users.noreply.github.com>
2022-07-26 01:03:35 +0300

    Edit menu

--- src/components/app-header.vue
+++ src/components/app-header.vue
@@ -9,27 +9,23 @@
     <stay-filter class="filter-header" @setFilter="setFilter" />
     <section class="user-actions flex align-items space-between">
       <div class="host">
-        <router-link :to="'/'" class="host-header link">Become a Host</router-link>
+        <router-link :to="'/'" class="host-header link"
+          >Become a Host</router-link
+        >
       </div>
-      <!-- <button class="btn ">
-        <img src="/footer-imgs/lan.png" alt="" />
-      </button> -->
       <section class="user-info flex">
         <button class="menu-btn btn">
-          <img class="menu-img" src="../../public/footer-imgs/menu.png" alt="" />
+          <img class="menu-img" src="../styles/icons/menu-icon.svg" alt="" />
         </button>
         <button class="user-btn btn">
-          <img class="user-img" src="../../public/footer-imgs/user.png" alt="" />
+          <img class="user-img" src="../styles/icons/user-icon.svg" alt="" />
         </button>
       </section>
     </section>
-
   </section>
-   
-  
 </template>
 <script>
-import stayFilter from "./stay-filter.vue"
+import stayFilter from "./stay-filter.vue";
 export default {
   props: [],
   template: ``,
@@ -37,11 +33,11 @@ export default {
     stayFilter,
   },
   data() {
-    return {}
+    return {};
   },
   methods: {},
   computed: {},
-  created() { },
-  unmounted() { },
-}
+  created() {},
+  unmounted() {},
+};
 </script>
